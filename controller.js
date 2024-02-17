86a0b4b59643ba8adf06752202ded27e7523cca0
Kaoutiz <94445745+Kaoutiz@users.noreply.github.com>
2024-02-17 17:44:30 +0100

    brayn-branch: adding login system

--- controller.js
+++ controller.js
@@ -2,9 +2,14 @@
 const bcrypt = require('bcrypt');
 const { User } = require('./db');
 
-async function createUser(req, res) {
+async function registerUser(req, res) {
     try {
         const { email, password } = req.body;
+
+        const existingUser = await User.findOne({ email });
+        if (existingUser) {
+            return res.status(400).json({ message: 'Cet email est déjà utilisé' });
+        }
 
         // Hasher le mot de passe
         const hashedPassword = await bcrypt.hash(password, 10);
@@ -19,6 +24,32 @@ async function createUser(req, res) {
     }
 }
 
+async function loginUser(req, res) {
+    try {
+        const { email, password } = req.body;
+
+        // Recherche de l'utilisateur par email
+        const user = await User.findOne({ email });
+
+        // Vérification si l'utilisateur existe
+        if (!user) {
+            return res.status(404).json({ message: 'Utilisateur non trouvé' });
+        }
+
+        // Vérification du mot de passe
+        const passwordMatch = await bcrypt.compare(password, user.password);
+        if (!passwordMatch) {
+            return res.status(401).json({ message: 'Mot de passe incorrect' });
+        }
+
+        res.status(200).json({ message: 'Connexion réussie', user });
+    } catch (error) {
+        res.status(500).json({ message: 'Erreur lors de la connexion', error: error.message });
+    }
+}
+
+
 module.exports = {
-    createUser
+    registerUser,
+    loginUser
 };
